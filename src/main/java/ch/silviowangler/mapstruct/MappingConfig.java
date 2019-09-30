e929caf4623fe9451571ced907cdd6c635cf568a
Silvio Wangler <silvio.wangler@gmail.com>
2019-09-30 11:31:56 +0200

    Correct the global mapping config

--- src/main/java/ch/silviowangler/mapstruct/MappingConfig.java
+++ src/main/java/ch/silviowangler/mapstruct/MappingConfig.java
@@ -2,12 +2,14 @@ package ch.silviowangler.mapstruct;
 
 import org.mapstruct.MapperConfig;
 import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 /**
  * @author Silvio Wangler
  */
 @MapperConfig(
-        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE
 )
 public class MappingConfig {
 }
